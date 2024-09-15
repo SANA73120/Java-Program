b15ca0546b8151c2edbbf07999f1bd058f855afb
Sana Khan <142918622+SANA73120@users.noreply.github.com>
2024-09-15 19:38:06 +0530

    Add files via upload
    Java program to create a class called "Rectangle" with width and height attributes. 
    Calculate the area and perimeter of the rectangle.

---
+++ prog1.java
@@ -0,0 +1,22 @@
+class rectangle{
+    double height;
+    double width;
+    rectangle(double height, double width){
+        this.height=height;
+        this.width=width;
+    }
+
+    void area(){
+        System.out.println("Area is",height*width);
+    }
+    void perimeter(){
+        System.out.println("Perimeter is",2*(height+width));
+    }
+}
+class main{
+    public static void main(String[] args){
+        rectangle r=new rectangle(2,4);
+	r.area();
+	r.perimeter();
+    }
+}
